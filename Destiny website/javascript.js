572c60041ba91d75bbae2c5c23aa981eb5e4c361
Destiny <dgriesgr@risd.edu>
2021-04-28 12:05:23 -0400

    shtufff

--- Destiny website/javascript.js
+++ Destiny website/javascript.js
@@ -1,99 +1,99 @@
 function zoom() {
     var x = document.getElementById("Zoom");
-    if (x.style.display === "none") 
+    if (x.style.display === "block") 
     {
-        x.style.display = "block";
+        x.style.display = "none";
     }
     else 
     {
-        x.style.display = "none";
+        x.style.display = "block";
     }
 }
 function pvd() {
     var x = document.getElementById("PVD");
-    if (x.style.display === "none") 
+    if (x.style.display === "block") 
     {
-        x.style.display = "block";
+        x.style.display = "none";
     }
     else 
     {
-        x.style.display = "none";
+        x.style.display = "block";
     }
 }
 function quarentine() {
     var x = document.getElementById("Quarentine");
-    if (x.style.display === "none") 
+    if (x.style.display === "block") 
     {
-        x.style.display = "block";
+        x.style.display = "none";
     }
     else 
     {
-        x.style.display = "none";
+        x.style.display = "block";
     }
 }
 function colaborative() {
     var x = document.getElementById("Colaborative");
-    if (x.style.display === "none") 
+    if (x.style.display === "block") 
     {
-        x.style.display = "block";
+        x.style.display = "none";
     }
     else 
     {
-        x.style.display = "none";
+        x.style.display = "block";
     }
 }
 function oneFish() {
     var x = document.getElementById("OneFish");
-    if (x.style.display === "none") 
+    if (x.style.display === "block") 
     {
-        x.style.display = "block";
+        x.style.display = "none";
     }
     else 
     {
-        x.style.display = "none";
+        x.style.display = "block";
     }
 }
 function bait() {
     var x = document.getElementById("Bait");
-    if (x.style.display === "none") 
+    if (x.style.display === "block") 
     {
-        x.style.display = "block";
+        x.style.display = "none";
     }
     else 
     {
-        x.style.display = "none";
+        x.style.display = "block";
     }
 }
 function faer() {
     var x = document.getElementById("Faer");
-    if (x.style.display === "none") 
+    if (x.style.display === "block") 
     {
-        x.style.display = "block";
+        x.style.display = "none";
     }
     else 
     {
-        x.style.display = "none";
+        x.style.display = "block";
     }
 }
 function riso() {
     var x = document.getElementById("RISO");
-    if (x.style.display === "none") 
+    if (x.style.display === "block") 
     {
-        x.style.display = "block";
+        x.style.display = "none";
     }
     else 
     {
-        x.style.display = "none";
+        x.style.display = "block";
     }
 }
 function abram() {
     var x = document.getElementById("Abram");
-    if (x.style.display === "none") 
+    if (x.style.display === "block") 
     {
-        x.style.display = "block";
+        x.style.display = "none";
     }
     else 
     {
-        x.style.display = "none";
+        x.style.display = "block";
     }
 }
